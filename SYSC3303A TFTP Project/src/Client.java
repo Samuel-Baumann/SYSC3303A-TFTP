e441e1620a18215be331f7737988f9f56deb7602
Sirak Berhane <sirakberhane2979@gmail.com>
2019-05-13 10:35:24 -0400

    Iteration #0
    Iteration #0 code

---
+++ SYSC3303A TFTP Project/src/Client.java
@@ -0,0 +1,121 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * 
+ * @author Group 5
+ * @version 5/11/2018 (Iteration #0)
+ * 
+ * Client-side Algorithm
+ *
+ */
+public class Client {	
+	private DatagramPacket sendPacket, receivePacket;
+	private DatagramSocket sendReceiveSocket;
+	public Client() {
+		try {
+			sendReceiveSocket = new DatagramSocket();
+		} catch (SocketException e){
+			print("CLIENT ERROR OCCURED: " + e.getStackTrace().toString());
+		}
+	}
+
+	public void sendReceivePackets() {
+		byte msg[] = new byte[100];
+		String filename = "";
+		String mode = "";
+
+		for (int i = 0; i < 11; i++) {
+			// Alternate between Read (even) and Write (odd) requests.
+			msg[0] = "0".getBytes()[0];
+			if (i % 2 == 0) { 	// Read Request
+				msg[1] = "1".getBytes()[0];
+			} else { 			// Write Request
+				msg[1] = "2".getBytes()[0];
+			} if (i == 11) { 
+				msg[1] = "5".getBytes()[0];		// Invalid Request occurs on the 11th request
+			}
+
+			// Convert filename from String to Byte[] and add it to message, then add 0 byte.
+			filename = "test.txt";
+			System.arraycopy(filename.getBytes(), 0, msg, 2, filename.getBytes().length);
+			msg[filename.getBytes().length + 2] = "0".getBytes()[0];
+
+			// Convert mode from String to Byte[] and add it to message, then add 0 byte.
+			mode = "netascii";
+			System.arraycopy(mode.getBytes(), 0, msg, filename.getBytes().length + 3, mode.getBytes().length);
+			int sizeOfMsg = filename.getBytes().length + mode.getBytes().length + 4;
+			msg[sizeOfMsg - 1] = "0".getBytes()[0];
+
+			// Send the packet to Host
+			try {
+				sendPacket = new DatagramPacket(msg, sizeOfMsg, InetAddress.getLocalHost(), 23);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Client: Sending packet.");
+			print("To host: " + sendPacket.getAddress());
+			print("Destination host port: " + sendPacket.getPort());
+			int len = sendPacket.getLength();
+			print("Length: " + len);
+			print("Containing (String): " + new String(sendPacket.getData(),0,len));
+			String byteData = "|";
+			for(int j = 0; j < sendPacket.getData().length; j++) {
+				byteData += sendPacket.getData()[j] + "|";
+			}
+			print("Containing (byte): " + byteData + "\n");
+
+			try {
+				sendReceiveSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Client: Packet sent to Host.\n");
+
+			// Receive a packet from Host
+			byte data[] = new byte[4];
+			receivePacket = new DatagramPacket(data, data.length);
+
+			print("Client: Waiting for packets to arrive.\n");
+
+			try {
+				sendReceiveSocket.receive(receivePacket);
+			} catch(IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Client: Packet received.");
+			print("From host: " + receivePacket.getAddress());
+			print("Host port: " + receivePacket.getPort());
+			print("Length: " + receivePacket.getLength());
+			print("Containing (String): " + new String(data,0,receivePacket.getLength()));
+			String byteReceivedData = "|";
+			for(int j = 0; j < receivePacket.getLength(); j++) {
+				byteReceivedData += data[j] + "|";
+			}
+			print("Containing (byte): " + byteReceivedData + "\n");
+		}
+		print("Client: Closing old sockets ...");
+		sendReceiveSocket.close();
+		print("[Done] Client: Closed old sockets");
+		System.exit(0);
+	}
+
+	private void print(String printable) {
+		System.out.println(printable);
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.sendReceivePackets();
+	}
+}
---
+++ SYSC3303A TFTP Project/src/Host.java
@@ -0,0 +1,145 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * 
+ * @author Group 5
+ * @version 5/11/2018 (Iteration #0)
+ * 
+ * Intermediate Host-side Algorithm
+ *
+ */
+public class Host {
+	private DatagramPacket sendPacket, receivePacket;
+	private DatagramSocket sendReceiveSocket, receiveSocket, sendSocket;
+	private byte dataRecieved[] = new byte[100];
+	private InetAddress clientAddress;
+	private int clientPort;
+	private int clientLength;
+
+
+	public Host() {
+		try {
+			receiveSocket = new DatagramSocket(23);
+			sendSocket = new DatagramSocket();
+			sendReceiveSocket = new DatagramSocket();
+			receiveSocket.setSoTimeout(60000);
+			sendSocket.setSoTimeout(60000);
+			sendReceiveSocket.setSoTimeout(60000);
+		} catch (SocketException e){
+			print("HOST ERROR OCCURED: " + e.getStackTrace().toString());
+		}
+	}
+
+	public void sendReceievePackets() {
+		// Repeat send and receive requests indefinitely 
+		for(;;) {
+			// Receive a request from Client Instance
+			print("Host: Waiting for packets to arrive.\n");
+			receivePacket = new DatagramPacket(dataRecieved, dataRecieved.length);
+
+			try {
+				receiveSocket.receive(receivePacket);
+			} catch(IOException e) {
+				print("Host: Closing all ports.");
+				System.exit(1);
+			}
+
+			print("Host: Packet received.");
+			print("From client: " + receivePacket.getAddress());
+			clientAddress = receivePacket.getAddress();
+			print("Host port: " + receivePacket.getPort());
+			clientPort = receivePacket.getPort();
+			print("Length: " + receivePacket.getLength());
+			clientLength = receivePacket.getLength();
+			print("Containing (String): " + new String(dataRecieved,0,receivePacket.getLength()));
+			String data = "|";
+			for(int j = 0; j < dataRecieved.length; j++) {
+				data += dataRecieved[j] + "|";
+			}
+			print("Containing (byte): " + data + "\n");
+
+			// Send the received packets to server at port 69
+			print("Host: Echo packets to server. \n");
+
+			try {
+				sendPacket = new DatagramPacket(dataRecieved, dataRecieved.length, InetAddress.getLocalHost(), 69);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Host: Sending packet.");
+			print("To server: " + sendPacket.getAddress());
+			print("Destination server port: " + sendPacket.getPort());
+			print("Length: " + sendPacket.getLength());
+			print("Containing (String): " + new String(dataRecieved,0,receivePacket.getLength()));
+			String dataToServer = "|";
+			for(int j = 0; j < dataRecieved.length; j++) {
+				dataToServer += dataRecieved[j] + "|";
+			}
+			print("Containing (byte): " + dataToServer + "\n");
+
+			try {
+				sendReceiveSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Host: Packet sent to Server.\n");
+
+			// Receive response from server
+			try {
+				sendReceiveSocket.receive(receivePacket);
+			} catch(IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			print("Host: Packet received.");
+			print("From server: " + receivePacket.getAddress());
+			print("Host port: " + receivePacket.getPort());
+			print("Length: " + receivePacket.getLength());
+			print("Containing (String): " + new String(dataRecieved,0,receivePacket.getLength()));
+			String dataServer = "|";
+			for(int j = 0; j < dataRecieved.length; j++) {
+				dataServer += dataRecieved[j] + "|";
+			}
+			print("Containing (byte): " + dataServer + "\n");
+
+			sendPacket = new DatagramPacket(dataRecieved, dataRecieved.length, clientAddress, clientPort);
+
+			print("Host: Sending packet.");
+			print("To client: " + clientAddress);
+			print("Destination client port: " + clientPort);
+			print("Length: " + clientLength);
+			print("Containing (String): " + new String(dataRecieved,0,receivePacket.getLength()));
+			String dataClient = "|";
+			for(int j = 0; j < dataRecieved.length; j++) {
+				dataClient += dataRecieved[j] + "|";
+			}
+			print("Containing (byte): " + dataClient + "\n");
+
+			try {
+				sendSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	private void print(String printable) {
+		System.out.println(printable);
+	}
+
+	public static void main(String[] args) {
+		Host host = new Host();
+		host.sendReceievePackets();
+	}
+}
