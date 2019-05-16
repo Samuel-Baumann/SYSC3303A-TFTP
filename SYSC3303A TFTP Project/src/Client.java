7aea97587466256c7c23a6cad8d24f4dc8df85c6
Sirak Berhane <sirakberhane2979@gmail.com>
2019-05-16 14:01:32 -0400

    Added Client text based UI

--- SYSC3303A TFTP Project/src/Client.java
+++ SYSC3303A TFTP Project/src/Client.java
@@ -3,7 +3,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -18,13 +17,17 @@ public class Client extends Thread{
 	private Print printable;
 	private Constants.ModeType mode;
 	private boolean requestedThreadClosing;
-	private Scanner in = new Scanner(System.in);
-	
+	private byte msg[] = new byte[18];
+	private static String filename = "";
+	private static String typeOfRequest = "";
+	private static String optionSelected = "";
+	private static Scanner in = new Scanner(System.in);
+
 	public Client(Constants.ModeType mode, boolean requestedThreadClosing) {
 		this.mode = mode;
-		this.setRequestedThreadClosing(requestedThreadClosing);
+		this.requestedThreadClosing = requestedThreadClosing;
 		this.printable = new Print(this.mode);
-
+		
 		try {
 			sendReceiveSocket = new DatagramSocket();
 		} catch (SocketException e){
@@ -37,59 +40,16 @@ public class Client extends Thread{
 			sendReceivePackets();
 		} catch (Exception e) {
 			print("[Client] Thread Error Occured: " + e.getMessage());
+			System.exit(1);
 		}
 	}
 
-	public void sendReceivePackets() throws UnknownHostException {
-		byte msg[] = new byte[18];
-		String filename = "";
+	public void sendReceivePackets() throws Exception {
 		String mode = "";
-		String typeOfRequest = "";
-		String optionSelected;
-
 		sendPacket = new DatagramPacket(new byte[18], 18, InetAddress.getLocalHost(), 23);
 		receivePacket = new DatagramPacket(new byte[1], 1);
 
 		for(;;) {
-			for(;;) {
-				if (requestedThreadClosing == true) {
-					break;
-				}
-				System.out.print("Enter (R) for read request, (W) for write request, or (E) for program termination: ");
-				optionSelected = in.nextLine();
-				if (optionSelected.toUpperCase().equals("R")) {
-					typeOfRequest = Constants.PacketString.RRQ.getPacketStringType();
-					print(typeOfRequest);
-					break;
-				} else if (optionSelected.toUpperCase().equals("W")) {
-					typeOfRequest = Constants.PacketString.WRQ.getPacketStringType();
-					break;
-				} else if (optionSelected.toUpperCase().equals("E")) {
-					print("Client: Shutdown requested to server");
-					this.setRequestedThreadClosing(true);
-					break;
-				} else {
-					print("Invalid input! Please select again.");
-				}
-			}
-
-			for(;;) {
-				if (requestedThreadClosing == true) {
-					break;
-				}
-				print("Enter file name: ");
-				filename = in.nextLine();
-				if (filename.toUpperCase().equals("E")) {
-					print("Client: Shutdown requested to server");
-					this.setRequestedThreadClosing(true);
-					break;
-				} else if (filename != "") {
-					break;
-				} else {
-					print("Invalid input! Please select again.");
-				}
-			}
-
 			if (requestedThreadClosing == true) {
 				msg = new String("CloseServerThreads").getBytes();
 				sendPacket.setData(msg);
@@ -104,7 +64,7 @@ public class Client extends Thread{
 				Thread.currentThread().interrupt();
 				break;
 			} else {
-				// Alternate between Read (even) and Write (odd) requests.
+				// Read (RRQ) or Write (WRQ) requests write format into packet data
 				byte data[] = filename.getBytes();
 				for(int i = 0; i < data.length; i++) {
 					msg[0] = Constants.PacketByte.ZERO.getPacketByteType();
@@ -129,6 +89,7 @@ public class Client extends Thread{
 
 					msg[i+2] = data[i];
 					msg[filename.getBytes().length + 2] = Constants.PacketByte.ZERO.getPacketByteType();
+
 					// Convert mode from String to Byte[] and add it to message, then add 0 byte.
 					mode = "netascii";
 					System.arraycopy(mode.getBytes(), 0, msg, filename.getBytes().length + 3, mode.getBytes().length);
@@ -136,7 +97,7 @@ public class Client extends Thread{
 					msg[sizeOfMsg - 1] = Constants.PacketByte.ZERO.getPacketByteType();;
 					sendPacket.setData(msg);
 				}
-				
+
 				// Add actual packet send or build logic
 				if (typeOfRequest.equals("RRQ")) {
 					print("[Client]: Reading file");
@@ -177,6 +138,48 @@ public class Client extends Thread{
 		System.exit(0);
 	}
 
+	public void getRequestTypeFromUser() throws IOException {		
+		do {
+			System.out.println("Enter (R) for read request, (W) for write request, or (E) for program termination: ");
+			optionSelected = in.nextLine().toUpperCase();
+			System.out.println("[Client] Requested inputted: " + optionSelected);
+		} while (!(optionSelected.equals("R") || optionSelected.equals("W") || optionSelected.equals("E")));
+
+		if (optionSelected.equals("R")) {
+			typeOfRequest = Constants.PacketString.RRQ.getPacketStringType();
+		} 
+		
+		if (optionSelected.toUpperCase().equals("W")) {
+			typeOfRequest = Constants.PacketString.WRQ.getPacketStringType();
+		} 
+		
+		if (optionSelected.toUpperCase().equals("E")) {
+			System.out.println("Client: Shutdown requested to server");
+			requestedThreadClosing = true;
+		}
+	}
+
+	public void getFileNameFromUser() throws IOException{
+		System.out.println("Enter [1] Default File Location on Server [2] Custom File Location on Server: \n");
+		int fileDefault = in.nextInt();
+
+		if (fileDefault == 1) {
+			do {				
+				System.out.println("Enter file name: ");
+				filename = in.nextLine();
+			} while (filename.length() == 0);
+		} else {
+			filename = "test.txt";
+		}
+
+		if (filename.toUpperCase().equals("E")) {
+			System.out.println("Client: Shutdown requested to server");
+			requestedThreadClosing = true;
+		} else {
+			System.out.println("[Client] Filename entered: " + filename);
+		}
+	}
+
 	private void print(String printable) {
 		if (mode == Constants.ModeType.VERBOSE) {
 			System.out.println(printable);
@@ -189,5 +192,21 @@ public class Client extends Thread{
 
 	public void setRequestedThreadClosing(boolean requestedThreadClosing) {
 		this.requestedThreadClosing = requestedThreadClosing;
+	}
+	
+	public void startClient(Constants.ModeType mode, boolean requestedThreadClosing) {
+		try {
+			getRequestTypeFromUser();
+		} catch (IOException e) {
+			print("[Client] Request type error occured: " + e.getMessage());
+		}
+		
+		try {
+			getFileNameFromUser();
+		} catch (Exception e) {
+			print("[Client] File type error occured: " + e.getMessage());
+		}
+		
+		Thread.currentThread().start();
 	}
 }
