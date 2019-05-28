576c83ec01e1c82afbe0b75f286bd75184ae9d61
Sirak Berhane <sirakberhane2979@gmail.com>
2019-05-28 11:05:27 -0400

    Added Henry's Code

---
+++ SYSC3303-TFTP/src/Client.java
@@ -0,0 +1,523 @@
+
+// TFTPClient.java
+// This class is the client side for a very simple assignment based on TFTP on
+// UDP/IP. The client uses one port and sends a read or write request and gets 
+// the appropriate response from the server.  No actual file transfer takes place.   
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+	private DatagramPacket sendPacket, receivePacket;
+	private DatagramSocket sendReceiveSocket;
+
+	// we can run in normal (send directly to server) or test
+	// (send to simulator) mode
+	public static enum Mode {
+		NORMAL, TEST
+	};
+
+	public Client() {
+		try {
+			// Construct a datagram socket and bind it to any available
+			// port on the local host machine. This socket will be used to
+			// send and receive UDP Datagram packets.
+			sendReceiveSocket = new DatagramSocket();
+		} catch (SocketException se) { // Can't create the socket.
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void sendAndReceive() {
+
+		// Scanner object used to receive input
+		Scanner scan = new Scanner(System.in);
+		String s;
+
+		boolean verbose = false, running = true;
+
+		byte[] msg = new byte[100], // message we send
+				fn, // filename as an array of bytes
+				md, // mode as an array of bytes
+				data; // reply as array of bytes
+		String mode; // filename and mode as Strings
+		int j, len, sendPort;
+
+		File directory;
+
+		System.out.println("Type in the filepath for the directory used");
+		s = scan.next();
+		directory = new File(s);
+
+		while ((!directory.exists()) && (!directory.isDirectory())) {
+			System.out.println("Invalid path or not a directory");
+			System.out.println("Type in the filepath for the directory used");
+			s = scan.next();
+			directory = new File(s);
+		}
+
+		// System.out.println(directory.getPath());
+		File[] contents = directory.listFiles();
+
+		// In the assignment, students are told to send to 23, so just:
+		// sendPort = 23;
+		// is needed.
+		// However, in the project, the following will be useful, except
+		// that test vs. normal will be entered by the user.
+		Mode run = Mode.TEST; // change to NORMAL to send directly to server
+
+		while (running) {
+			System.out.println("enter a v for Verbose mode or a q for quiet mode: ");
+			s = scan.next();
+
+			if (s.compareToIgnoreCase("v") == 0) {
+				verbose = true;
+			}
+
+			System.out.println("enter a n for normal mode or a t for test mode: ");
+			s = scan.next();
+
+			if (s.compareToIgnoreCase("n") == 0) {
+				run = Mode.NORMAL;
+			}
+
+			if (run == Mode.NORMAL)
+				sendPort = 69;
+			else
+				sendPort = 23;
+
+			System.out.println("enter a y to display the directory or anything else not to: ");
+			s = scan.next();
+
+			if (s.compareToIgnoreCase("y") == 0) {
+				System.out.println("Directory contains");
+				for (File object : contents) {
+					if (object.isDirectory()) {
+						System.out.println("Directory name: " + object.getName());
+					}
+					if (object.isFile()) {
+						System.out.println("File name: " + object.getName());
+					}
+				}
+			}
+
+			System.out.println("enter a 1 for a read request or a 2 for a write request: ");
+			s = scan.next();
+
+			if (s.compareTo("1") == 0) {
+				msg[1] = 1;
+			} else if (s.compareTo("2") == 0) {
+				msg[1] = 2;
+			}
+
+			while ((s.compareTo("1") != 0) && (s.compareTo("2") != 0)) {
+				System.out.println("enter a 1 for a read request or a 2 for a write request: ");
+				s = scan.next();
+
+				if (s.compareTo("1") == 0) {
+					msg[1] = 1;
+				} else if (s.compareTo("2") == 0) {
+					msg[1] = 2;
+				}
+			}
+
+			if (s.compareTo("1") == 0) {
+				System.out.println("Forming a RRQ connection");
+				System.out.println("Client: creating packet . . .");
+
+				System.out.println("Type in the filename: ");
+				s = scan.next();
+
+				// Prepare a DatagramPacket and send it via sendReceiveSocket
+				// to sendPort on the destination host (also on this machine).
+
+				msg[0] = 0;
+
+				// extract filename and convert it to bytes
+				fn = s.getBytes();
+				// copy it into the msg
+				System.arraycopy(fn, 0, msg, 2, fn.length);
+				// insert 0 after filename
+				msg[fn.length + 2] = 0;
+
+				// now add "octet" (or "netascii")
+				mode = "octet";
+				// convert to bytes
+				md = mode.getBytes();
+
+				// and copy into the msg
+				System.arraycopy(md, 0, msg, fn.length + 3, md.length);
+
+				// length of the message
+				len = fn.length + md.length + 4;
+				// length of filename + length of mode + opcode (2) + two 0s (2)
+				// second 0 to be added next:
+
+				// end with another 0 byte
+				msg[len - 1] = 0;
+
+				// Construct a datagram packet that is to be sent to a specified port
+				// on a specified host.
+				try {
+					sendPacket = new DatagramPacket(msg, len, InetAddress.getLocalHost(), sendPort);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				System.out.println("Client: packet created");
+				System.out.println("Client: sending packet . . .");
+
+				if (verbose == true) {
+					System.out.println("To host: " + sendPacket.getAddress());
+					System.out.println("Destination host port: " + sendPacket.getPort());
+					len = sendPacket.getLength();
+					System.out.println("Length: " + len);
+					System.out.println("Containing: ");
+					for (j = 0; j < len; j++) {
+						System.out.println("byte " + j + " " + msg[j]);
+					}
+
+					// Form a String from the byte array, and print the string.
+					String sending = new String(msg, 0, len);
+					System.out.println(sending);
+				}
+
+				// Send the datagram packet to the server via the send/receive socket.
+
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				System.out.println("Client: Packet sent.");
+
+				int size = 512;
+				byte[] receivingArray = new byte[65535 * 512];
+				int blockNum = 1;
+
+				while (size == 512) {
+					data = new byte[516];
+					receivePacket = new DatagramPacket(data, data.length);
+
+					System.out.println("Client: Waiting for packet. . .");
+					try {
+						// Block until a datagram is received via sendReceiveSocket.
+						sendReceiveSocket.receive(receivePacket);
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(1);
+					}
+
+					// Process the received datagram.
+					System.out.println("Client: Packet received:");
+					if (verbose == true) {
+						System.out.println("From host: " + receivePacket.getAddress());
+						System.out.println("Host port: " + receivePacket.getPort());
+						len = receivePacket.getLength();
+						System.out.println("Length: " + len);
+						System.out.println("Block Num: "+blockNum);
+					}
+					System.out.println("Containing: "+(new String(data)));
+					size = receivePacket.getLength()-4;
+					System.arraycopy(data, 4, receivingArray, (blockNum-1)*512, size);
+
+					System.out.println("Creating packet . . .");
+
+					msg = new byte[4];
+					msg[0] = 0;
+					msg[1] = 4;
+					msg[2] = (byte) ((int) blockNum/256);
+					msg[3] = (byte) ((int) blockNum%256);
+
+					len = msg.length;
+					blockNum++;
+
+					try {
+						sendPacket = new DatagramPacket(msg, len, InetAddress.getLocalHost(), receivePacket.getPort());
+					} catch (UnknownHostException e) {
+						e.printStackTrace();
+						System.exit(1);
+					}
+
+					System.out.println("Packet created");
+					System.out.println("Sending packet . . .");
+					if (verbose == true) {
+						System.out.println("To host: " + sendPacket.getAddress());
+						System.out.println("Destination host port: " + sendPacket.getPort());
+						len = sendPacket.getLength();
+						System.out.println("Length: " + len);
+						System.out.println("Containing: ");
+						for (j = 0; j < len; j++) {
+							System.out.println("byte " + j + " " + msg[j]);
+						}
+
+						// Form a String from the byte array, and print the string.
+						String sending = new String(msg, 0, len);
+						System.out.println(sending);
+					}
+
+					// Send the datagram packet to the server via the send/receive socket.
+
+					try {
+						sendReceiveSocket.send(sendPacket);
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(1);
+					}
+
+				}
+
+				String Filepath = "./Client/"+(new String(fn));
+				File file = new File(Filepath);
+				try {
+					OutputStream os = new FileOutputStream(file);
+					os.write(receivingArray);
+					os.close();
+					System.out.println("TEST TEST TEST!!!!!!!!");
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+
+
+			} else if (s.compareTo("2") == 0) {
+				System.out.println("Forming a WRQ connection");
+				System.out.println("Client: creating packet . . .");
+
+				System.out.println("Type in the filename: ");
+				s = scan.next();
+
+				File sendingFile = new File(directory.getPath() + "\\" + s);
+
+				while (!sendingFile.exists()) {
+					System.out.println("Type in the filename: ");
+					s = scan.next();
+					sendingFile = new File(directory.getPath() + "\\" + s);
+				}
+
+				// Prepare a DatagramPacket and send it via sendReceiveSocket
+				// to sendPort on the destination host (also on this machine).
+
+				msg[0] = 0;
+
+				// extract filename and convert it to bytes
+				fn = sendingFile.getName().getBytes();
+				// copy it into the msg
+				System.arraycopy(fn, 0, msg, 2, fn.length);
+				// insert 0 after filename
+				msg[fn.length + 2] = 0;
+
+				// now add "octet" (or "netascii")
+				mode = "octet";
+				// convert to bytes
+				md = mode.getBytes();
+
+				// and copy into the msg
+				System.arraycopy(md, 0, msg, fn.length + 3, md.length);
+
+				// length of the message
+				len = fn.length + md.length + 4;
+				// length of filename + length of mode + opcode (2) + two 0s (2)
+				// second 0 to be added next:
+
+				// end with another 0 byte
+				msg[len - 1] = 0;
+
+				// Construct a datagram packet that is to be sent to a specified port
+				// on a specified host.
+				try {
+					sendPacket = new DatagramPacket(msg, len, InetAddress.getLocalHost(), sendPort);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				System.out.println("Client: packet created");
+				System.out.println("Client: sending packet . . .");
+
+				if (verbose == true) {
+					System.out.println("To host: " + sendPacket.getAddress());
+					System.out.println("Destination host port: " + sendPacket.getPort());
+					len = sendPacket.getLength();
+					System.out.println("Length: " + len);
+					System.out.println("Containing: ");
+					for (j = 0; j < len; j++) {
+						System.out.println("byte " + j + " " + msg[j]);
+					}
+
+					// Form a String from the byte array, and print the string.
+					String sending = new String(msg, 0, len);
+					System.out.println(sending);
+				}
+
+				// Send the datagram packet to the server via the send/receive socket.
+
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				System.out.println("Client: Packet sent.");
+
+				// Construct a DatagramPacket for receiving packets up
+				// to 100 bytes long (the length of the byte array).
+
+				data = new byte[100];
+				receivePacket = new DatagramPacket(data, data.length);
+
+				System.out.println("Client: Waiting for packet. . .");
+				try {
+					// Block until a datagram is received via sendReceiveSocket.
+					sendReceiveSocket.receive(receivePacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+
+				// Process the received datagram.
+				System.out.println("Client: Packet received:");
+
+				if (verbose == true) {
+					System.out.println("From host: " + receivePacket.getAddress());
+					System.out.println("Host port: " + receivePacket.getPort());
+					len = receivePacket.getLength();
+					System.out.println("Length: " + len);
+				}
+				System.out.println("Containing: "+(new String(data)));
+
+				long size = sendingFile.length();
+				int blockNum = 1;
+				FileInputStream fis = null;
+				byte[] fileArray = new byte[(int) sendingFile.length()];
+				try {
+					fis = new FileInputStream(sendingFile);
+					fis.read(fileArray);
+					fis.close();
+				} catch (IOException ioExp) {
+					ioExp.printStackTrace();
+					System.exit(1);
+				}
+
+				while ((blockNum-1)*512 < fileArray.length) {
+					if (blockNum > 65535) {
+						System.out.println(new Exception("The file is too large to send"));
+						System.exit(1);
+					} else {
+						System.out.println("Client: creating packet . . .");
+
+//						msg = new byte[516];
+//						msg[0] = 0;
+//						msg[1] = 3;
+//						msg[2] = (byte) ((int) blockNum/256);
+//						msg[3] = (byte) ((int) blockNum%256);
+
+						if ((blockNum)*512<fileArray.length) {
+							msg = new byte[516];
+							msg[0] = 0;
+							msg[1] = 3;
+							msg[2] = (byte) ((int) blockNum/256);
+							msg[3] = (byte) ((int) blockNum%256);
+							System.arraycopy(fileArray, (blockNum-1)*512, msg, 4, 512);
+							sendPacket = new DatagramPacket(msg, msg.length, 
+									receivePacket.getAddress(), receivePacket.getPort());
+						} else {
+							msg = new byte[(fileArray.length % 512)+4];
+							msg[0] = 0;
+							msg[1] = 3;
+							msg[2] = (byte) ((int) blockNum/256);
+							msg[3] = (byte) ((int) blockNum%256);
+							 System.arraycopy(fileArray, (blockNum-1)*512, msg, 4, fileArray.length % 512);
+				                sendPacket = new DatagramPacket(msg, fileArray.length % 512,
+				                		receivePacket.getAddress(), receivePacket.getPort());
+						}
+						//blockNum++;
+
+						sendPacket = new DatagramPacket(msg, msg.length, receivePacket.getAddress(), receivePacket.getPort());
+
+						System.out.println("Client: packet created");
+						System.out.println("Client: sending packet . . .");
+
+						if (verbose == true) {
+							System.out.println("To host: " + sendPacket.getAddress());
+							System.out.println("Destination host port: " + sendPacket.getPort());
+							len = sendPacket.getLength();
+							System.out.println("block Number: "+blockNum);
+							System.out.println("Length: " + len);
+							System.out.println("Containing: "+new String(sendPacket.getData()));
+						}
+
+						// Send the datagram packet to the server via the send/receive socket.
+
+						try {
+							sendReceiveSocket.send(sendPacket);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(1);
+						}
+
+						System.out.println("Client: Packet sent.");
+
+						// Construct a DatagramPacket for receiving packets up
+						// to 100 bytes long (the length of the byte array).
+
+						data = new byte[100];
+						receivePacket = new DatagramPacket(data, data.length);
+
+						System.out.println("Client: Waiting for packet. . .");
+						try {
+							// Block until a datagram is received via sendReceiveSocket.
+							sendReceiveSocket.receive(receivePacket);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(1);
+						}
+
+						// Process the received datagram.
+						System.out.println("Client: Packet received:");
+
+						if (verbose == true) {
+							System.out.println("From host: " + receivePacket.getAddress());
+							System.out.println("Host port: " + receivePacket.getPort());
+							len = receivePacket.getLength();
+							System.out.println("Length: " + len);
+						}
+						System.out.println("Containing: "+(new String(receivePacket.getData())));
+
+						blockNum++;
+					}
+				}
+
+			}
+
+			// TODO: Continuous Input ..
+			System.out.println("Enter y to shut down or anything else to continue: ");
+			s = scan.next();
+
+			if (s.compareToIgnoreCase("y") == 0) {
+				running = false;
+			}
+
+			System.out.println();
+		}
+
+		// close scanner
+		scan.close();
+
+		// We're finished, so close the socket.
+		sendReceiveSocket.close();
+	}
+
+	public static void main(String args[]) {
+		Client c = new Client();
+		c.sendAndReceive();
+	}
+}
---
+++ SYSC3303-TFTP/src/Server.java
@@ -0,0 +1,301 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.file.*;
+
+public class Server{
+    DatagramSocket receiveSocket;
+    DatagramPacket receivePacket;
+
+    public Server(){
+        try{
+            receiveSocket = new DatagramSocket(69);
+        }catch(SocketException se){
+            se.printStackTrace();
+        }
+    }
+
+    public void receivingNewPacket(){
+        
+
+        while(true){
+            byte [] data = new byte[100]; 
+            receivePacket = new DatagramPacket(data,data.length);
+
+            // Server listen on port 69 forever (for now, in later iteration this needs to change)
+            // Server receives on port 69
+            System.out.println("******Server Waiting for Packet****\n");
+            try{
+                receiveSocket.receive(receivePacket);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+            }
+
+            System.out.println("New Packet Received!!");
+            System.out.println("From Host: "+receivePacket.getAddress());
+            System.out.println("Host Port: "+receivePacket.getPort());
+            System.out.println("Length: "+receivePacket.getLength());
+            System.out.println("Containing: ");
+
+            for(int x = 0; x<receivePacket.getLength(); x++){
+                System.out.print(data[x]+" ");
+            }
+
+            System.out.println("\n====================================");
+
+            // Server decides if this is a read/write request
+            // Server creates Thread to deal with Request and gives it the type
+            Thread dealWithClientRequest = new DealWithClientRequest(receivePacket, processingReadOrWrite(data));
+            dealWithClientRequest.start();
+
+            data = new byte[100]; 
+            receivePacket = new DatagramPacket(data,data.length);
+        }
+    }
+
+    // Processes the type of the request
+    // If it is anything besides read/write it quites
+    private String processingReadOrWrite(byte [] data){
+        if(data[0]!=0 || (data[1]!=1 && data[1]!=2)){
+            System.out.print(new Exception("ERROR: unknown request type."));
+            System.exit(1);
+        }
+
+        if(data[1]==1)
+            return "READ";
+        return "WRITE";
+    }
+
+    public static void main(String [] args){
+        Server serv = new Server();
+        serv.receivingNewPacket();
+    }
+}
+
+// New Thread which deals with Client Request
+class DealWithClientRequest extends Thread{
+    DatagramSocket sendReceiveSocket;
+    DatagramPacket receivePacket, sendPacket;
+    String type;
+    public DealWithClientRequest(DatagramPacket pckt, String type){
+        this.receivePacket = pckt;
+        this.type = type;
+
+        try{
+            sendReceiveSocket = new DatagramSocket();
+        }catch(SocketException se){
+            se.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public String[] getFilenameAndMode(){
+       
+        // temp[0] => filename
+        // temp[1] => mode
+        byte [] data = receivePacket.getData();
+        String [] temp = new String [2];
+        int len = receivePacket.getLength();
+        int x;
+
+        for(x = 2; x<len; x++){
+            if(data[x]==0) break;
+        }
+
+        if(x==len || x==2) {
+            System.out.println(new Exception("Filename not provided Or data not properly formatted"));
+            System.exit(1);
+        }
+
+        temp[0] = new String(data, 2, x-2);
+        int y;
+        for(y=x+1; y<len; y++){
+            if(data[y]==0)break;
+        }
+
+        if(y==x+1 || y==len){
+            System.out.println(new Exception("Mode not provided Or data not properly formatted"));
+            System.exit(1);
+        }
+
+        temp[1] = new String(data, y, y-x-1);
+
+        return temp;
+    }
+
+    public void run(){
+        System.out.println("New Thread with Packet: "+receivePacket+" of Type: "+type);
+
+        
+        
+        String [] information = getFilenameAndMode();
+        System.out.println("Filename: "+information[0]+" Mode:"+information[1]);
+        
+        // for(byte e: receivePacket.getData()){
+        //     System.out.print(e);
+        // }
+
+        if(type.equals("READ")) communicateReadRequest(information[0]);
+        else if(type.equals("WRITE")) communicateWriteRequest(information[0]);
+    }
+
+    public void communicateReadRequest(String filename){
+        File fn = new File("./Server/"+filename);
+
+        if(!fn.exists()){
+            System.out.println(new Exception("The file: temp/"+filename+" doesn't exists!"));
+            System.exit(1);
+        }
+
+        byte [] wholeBlock = null;
+        try{
+            wholeBlock = Files.readAllBytes(fn.toPath());
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+            System.exit(1);
+        }
+
+        // for(byte e: wholeBlock){
+        //     System.out.println(e+" ");
+        // }
+
+        int blockNum = 1;
+        int index = 0;
+        while((blockNum-1)*512 < wholeBlock.length){
+            byte [] msg = new byte [516];
+            int ind = 0;
+
+            msg[ind++] = 0;
+            msg[ind++] = 3;
+            msg[ind++] = (byte)(blockNum/256);
+            msg[ind++] = (byte)(blockNum%256);
+
+            if((blockNum)*512<wholeBlock.length){
+                System.arraycopy(wholeBlock, (blockNum-1)*512, msg, 4, 512);
+                sendPacket = new DatagramPacket(msg, msg.length, receivePacket.getAddress(), receivePacket.getPort());
+            }else{
+                System.arraycopy(wholeBlock, (blockNum-1)*512, msg, 4, wholeBlock.length % 512);
+                sendPacket = new DatagramPacket(msg, wholeBlock.length % 512, receivePacket.getAddress(), receivePacket.getPort());
+            }
+                
+            System.out.println( "Creating DATA Packet to Send"+
+                                "\nClient Address: "+ sendPacket.getAddress()+
+                                "\nClient Port: "+ sendPacket.getPort()+
+                                "\nBlock Number: "+ blockNum+
+                                "\nCONTAINS: "+new String(sendPacket.getData(), 4, ((blockNum)*512<wholeBlock.length)?512:wholeBlock.length % 512));
+
+            try{
+                sendReceiveSocket.send(sendPacket);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+                System.exit(1);
+            }
+
+            try{
+                sendReceiveSocket.receive(receivePacket);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+                System.exit(1);
+            }
+
+            int clientBlockNum = (256*receivePacket.getData()[2])+receivePacket.getData()[3];
+            System.out.println(clientBlockNum);
+
+            if(clientBlockNum != blockNum){
+                System.out.println(new Exception("Client & Server block number missmatch!"));
+            }
+
+            //System.out.println("******New Packet Recieved*****");
+
+            blockNum++;
+        }
+
+        System.out.println("THREAD TERMINATED");
+    }
+
+    public void communicateWriteRequest(String filename){
+        byte [] receivedBytes = new byte[65535 * 512];
+        int receivedBytesIndex = 0;
+        int blockNum = 0;
+        int blockSize = 1000000;
+
+        byte [] msg = new byte [4];
+        msg[0] = 0;
+        msg[1] = 4;
+        msg[2] = 0;
+        msg[3] = 0; // Ack blk# = 0
+        
+        sendPacket = new DatagramPacket(msg, msg.length, receivePacket.getAddress(), receivePacket.getPort());
+
+        System.out.println( "Creating ACK Packet to Send"+
+                                "\nClient Address: "+ sendPacket.getAddress()+
+                                "\nClient Port: "+ sendPacket.getPort()+
+                                "\nBlock Number: "+ blockNum);
+
+        try{
+            sendReceiveSocket.send(sendPacket);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+            System.exit(1);
+        }
+
+        blockNum++;
+        receivePacket = new DatagramPacket(new byte[516], 516, 
+        		receivePacket.getAddress(), receivePacket.getPort());
+
+        while(blockSize >= 516){
+            
+            System.out.println("Waiting To Receive Data package...");
+            try{
+                sendReceiveSocket.receive(receivePacket);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+                System.exit(1);
+            }
+
+            System.out.println( "Got DATA Packet from Client"+
+                                "\nClient Address: "+ receivePacket.getAddress()+
+                                "\nClient Port: "+ receivePacket.getPort()+
+                                "\nBlock Number: "+ blockNum+
+                                "\nLength: "+receivePacket.getLength()+
+                                "\nCONTAINS: "+new String(receivePacket.getData(), 4, receivePacket.getLength()-4));
+
+            byte [] data = receivePacket.getData();            
+            int clientBlockNum = (256*data[2])+data[3];
+            if(data[0]==0 && data[1]==3 && blockNum == clientBlockNum){
+                System.arraycopy(data, 4, receivedBytes, (blockNum-1)*512, receivePacket.getLength()-4);
+            }
+
+            blockSize = receivePacket.getLength();
+
+            msg[2] = (byte) ((int)blockNum/256);
+            msg[3] = (byte) ((int)blockNum%256);
+
+            sendPacket = new DatagramPacket(msg, msg.length, receivePacket.getAddress(), receivePacket.getPort());
+
+            try{
+                sendReceiveSocket.send(sendPacket);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+                System.exit(1);
+            }
+
+            blockNum++;
+        }
+
+        String Filepath = "./Server/"+(new String(filename));
+            File file = new File(Filepath);
+            try {
+                OutputStream os = new FileOutputStream(file);
+                os.write(receivedBytes);
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        
+        System.out.println("THREAD TERMINATED");
+    }
+}
+
+ 
